b31b8289ca20000e94b7b5f2a8e37cbb7d294fc1
VIGNESH-BOOPATHI <157092952+VIGNESH-BOOPATHI@users.noreply.github.com>
2024-02-26 12:27:55 +0530

    this is the task of day 16 (Call-Back-Hell)

---
+++ script.js
@@ -0,0 +1,36 @@
+var h1 = document.createElement('h1');
+document.body.append(h1);
+
+setTimeout(function() {
+    document.querySelector('h1').innerText = ` ${10}`;
+    setTimeout(function() {
+        document.querySelector('h1').innerText = ` ${9}`;
+        setTimeout(function() {
+            document.querySelector('h1').innerText = ` ${8}`;
+            setTimeout(function() {
+                document.querySelector('h1').innerText = ` ${7}`;
+                setTimeout(function() {
+                    document.querySelector('h1').innerText = ` ${6}`;
+                    setTimeout(function() {
+                        document.querySelector('h1').innerText = ` ${5}`;
+                        setTimeout(function() {
+                            document.querySelector('h1').innerText = ` ${4}`;
+                            setTimeout(function() {
+                                document.querySelector('h1').innerText = ` ${3}`;
+                                setTimeout(function() {
+                                    document.querySelector('h1').innerText = ` ${2}`;
+                                    setTimeout(function() {
+                                        document.querySelector('h1').innerText = ` ${1}`;
+                                        setTimeout(function() {
+                                            document.querySelector('h1').innerText = `Happy Independence Day`;
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
